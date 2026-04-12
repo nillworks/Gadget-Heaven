89ea575dbc2a4eaa2102685d214e8e90c2d0dbac
Shipon Roy <shiponworks@gmail.com>
2026-04-12 17:10:29 +0600

    fix: resolve merge conflict

--- src/Components/Products/ProductDetails.jsx
+++ src/Components/Products/ProductDetails.jsx
@@ -1,5 +1,5 @@
-import { LucideHeart, LucideShoppingCart, LucideStar } from "lucide-react";
-import React from "react";
+import { LucideHeart, LucideShoppingCart, LucideStar } from 'lucide-react';
+import React from 'react';
 
 const ProductDetails = () => {
   return (
@@ -9,7 +9,7 @@ const ProductDetails = () => {
           <div className="text-center pt-8 pb-8">
             <h1 className="font-bold text-white text-3xl">Product Details</h1>
             <p className="text-base text-white">
-              Explore the latest gadgets that will take your experience to{" "}
+              Explore the latest gadgets that will take your experience to{' '}
               <br />
               the next level. From smart devices to the coolest accessories, we
               have it all!
