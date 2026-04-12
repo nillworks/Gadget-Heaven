d7e12de728ebf32c71e805656071806a05304fc0
JowelislamHabib <jowel@bintofajjal.com>
2026-04-12 12:23:44 +0600

    feat: Add ProductDetails component and update layout and page structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import HomePage from '@/Components/AllPages/HomePage/HomePage';
+import HomePage from "@/Components/AllPages/HomePage/HomePage";
+import ProductDetails from "@/Components/Products/ProductDetails";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
+      <ProductDetails />
     </div>
   );
 }
